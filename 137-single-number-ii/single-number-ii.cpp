6b2eabc799f5b01e513993951c79f41c81597ab5
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-06-25 22:47:43 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (36.89%) - LeetSync

---
+++ 137-single-number-ii/single-number-ii.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int result = 0;
+        
+        for (int i = 0; i < 32; i++) {
+            int count = 0;
+            
+            for (int num : nums) {
+                if (num & (1 << i)) {
+                    count++;
+                }
+            }
+            
+            if (count % 3 != 0) {
+                result |= (1 << i);
+            }
+        }
+        
+        return result;
+    }
+};
+
+class SolutionOptimized {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        
+        for (int num : nums) {
+            twos |= (ones & num);
+            ones ^= num;
+            int threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        
+        return ones;
+    }
+};
