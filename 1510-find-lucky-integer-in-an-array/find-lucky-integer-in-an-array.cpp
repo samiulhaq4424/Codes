44864bc12f81cfb932261bd5a6a326aa3690ce79
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-07-05 19:42:27 +0530

    Time: 0 ms (100.00%) | Memory: 14 MB (37.49%) - LeetSync

---
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findLucky(vector<int>& arr) {
+        unordered_map<int, int> freq;
+        for (int num : arr) {
+            freq[num]++;
+        }
+        
+        int maxLucky = -1;
+        for (const auto& [num, count] : freq) {
+            if (num == count) {
+                maxLucky = max(maxLucky, num);
+            }
+        }
+        
+        return maxLucky;
+    }
+};
