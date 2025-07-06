13dd444608950583178ee808a833d1466538497c
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-07-06 16:25:57 +0530

    Time: 0 ms (100%), Space: 17.7 MB (70.82%) - LeetArchive

---
+++ LeetCode/0393-utf-8-validation/0393-utf-8-validation.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    bool validUtf8(vector<int>& data) {
+        int remainingBytes = 0;
+        
+        for (int byte : data) {
+            if (remainingBytes == 0) {
+                if ((byte >> 7) == 0) {
+                    remainingBytes = 0;
+                } else if ((byte >> 5) == 0b110) {
+                    remainingBytes = 1;
+                } else if ((byte >> 4) == 0b1110) {
+                    remainingBytes = 2;
+                } else if ((byte >> 3) == 0b11110) {
+                    remainingBytes = 3;
+                } else {
+                    return false;
+                }
+            } else {
+                if ((byte >> 6) != 0b10) {
+                    return false;
+                }
+                remainingBytes--;
+            }
+        }
+        
+        return remainingBytes == 0;
+    }
+};
