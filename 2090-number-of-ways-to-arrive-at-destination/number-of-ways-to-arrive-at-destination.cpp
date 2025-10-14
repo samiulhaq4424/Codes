9dfec179b660f91fb92d4d3e92c31a3255fadc9d
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-10-14 22:38:04 +0530

    Time: 13 ms (33.34%) | Memory: 37.2 MB (46.07%) - LeetSync

--- 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
+++ 2090-number-of-ways-to-arrive-at-destination/number-of-ways-to-arrive-at-destination.cpp
@@ -1,41 +1,40 @@
 class Solution {
 public:
     int countPaths(int n, vector<vector<int>>& roads) {
-        const int mod = 1e9 + 7;
+        int mod = (int)(1e9+7);
         vector<vector<pair<int,int>>> adj(n);
-        for (auto &it : roads) {
+        for(auto it : roads){
             int u = it[0];
             int v = it[1];
             int wt = it[2];
-            adj[u].push_back({v, wt});
-            adj[v].push_back({u, wt});
+            adj[u].push_back({v,wt});
+            adj[v].push_back({u,wt});
         }
 
         priority_queue<pair<long long,int>, vector<pair<long long,int>>, greater<pair<long long,int>>> pq;
-        vector<long long> dist(n, LLONG_MAX);
-        vector<int> ways(n, 0);
-        dist[0] = 0;
-        ways[0] = 1;
-        pq.push({0, 0});
+        vector<long long> dist(n,LLONG_MAX);
+        vector<long long> ways(n,0);
+        dist[0]=0;
+        ways[0]=1;
+        pq.push({0,0});
 
-        while (!pq.empty()) {
-            auto [dis, u] = pq.top();
+        while(!pq.empty()){
+            auto it = pq.top();
             pq.pop();
-
-            if (dis > dist[u]) continue; // skip stale
-
-            for (auto [v, wt] : adj[u]) {
-                long long newDist = dis + 1LL * wt;
-                if (newDist < dist[v]) {
-                    dist[v] = newDist;
-                    ways[v] = ways[u];
-                    pq.push({dist[v], v});
-                } else if (newDist == dist[v]) {
-                    ways[v] = (ways[v] + ways[u]) % mod;
+            long long dis = it.first;
+            int u = it.second;
+            for(auto itr : adj[u]){
+                int v = itr.first;
+                int wt = itr.second;
+                if(dis + 1LL * wt < dist[v]){
+                    ways[v]=ways[u];
+                    dist[v]=dis+wt;
+                    pq.push({dist[v],v});
+                } else if(dis + wt == dist[v]){
+                    ways[v] = (ways[u] + ways[v])%mod;
                 }
             }
         }
-
-        return ways[n - 1] % mod;
+        return ways[n-1]%mod;
     }
 };
