3f675e40686fdd70aee4f810223530d56ae448ec
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-10-14 20:23:17 +0530

    Time: 52 ms (61.94%) | Memory: 24.3 MB (97.33%) - LeetSync

--- 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
+++ 1753-path-with-minimum-effort/path-with-minimum-effort.cpp
@@ -5,10 +5,9 @@ public:
         int m = heights[0].size();
 
         vector<vector<int>> dist(n, vector<int>(m, 1e9));
-        priority_queue < pair<int, pair<int, int>>,
-            vector<pair<int, pair<int, int>>>,
-                   greater<pair<int, pair<int, int>>>>
-                pq;
+        priority_queue < pair<int, pair<int, int>>,vector<pair<int, pair<int, int>>>,
+                   greater<pair<int, pair<int, int>>>> pq;
+        
         pq.push({0, {0, 0}});
         dist[0][0] = 0;
 
