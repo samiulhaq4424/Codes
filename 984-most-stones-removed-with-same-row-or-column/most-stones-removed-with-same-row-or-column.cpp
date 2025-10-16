dbeb5d4f9f46e12b2e932b9cb18776af9b9f8444
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2025-10-16 22:17:14 +0530

    Time: 19 ms (59.78%) | Memory: 24.7 MB (28.39%) - LeetSync

---
+++ 984-most-stones-removed-with-same-row-or-column/most-stones-removed-with-same-row-or-column.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DisjointSet {
+private:
+    vector<int> parent, size;
+
+public:
+    DisjointSet(int n) {
+        parent.resize(n);
+        size.resize(n, 1);
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+    }
+
+    int findParent(int u) {
+        if (u == parent[u]) return u;
+        return parent[u] = findParent(parent[u]);
+    }
+
+    void unionBySize(int u, int v) {
+        int ulp_u = findParent(u);
+        int ulp_v = findParent(v);
+        if (ulp_u == ulp_v) return;
+
+        if (size[ulp_u] >= size[ulp_v]) {
+            parent[ulp_v] = ulp_u;
+            size[ulp_u] += size[ulp_v];
+        } else {
+            parent[ulp_u] = ulp_v;
+            size[ulp_v] += size[ulp_u];
+        }
+    }
+};
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        int numOfStones = stones.size();
+        int maxRow = INT_MIN;
+        int maxColumn = INT_MIN;
+        for (auto it : stones) {
+            maxRow = max(maxRow, it[0]);
+            maxColumn = max(maxColumn, it[1]);
+        }
+
+        DisjointSet ds(maxRow + maxColumn + 2);
+        unordered_map<int, int> mp;
+        for (auto it : stones) {
+            int row = it[0];
+            int column = maxRow + 1 + it[1];
+            ds.unionBySize(row, column);
+            mp[row] = 1;
+            mp[column] = 1;
+        }
+
+        int numOfComp = 0;
+        for (auto it : mp) {
+            if (ds.findParent(it.first) == it.first) {
+                ++numOfComp;
+            }
+        }
+
+        return numOfStones - numOfComp;
+    }
+};
