7b38a9dacba916e047c19552b2d9808f23aefee3
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-06-28 19:22:34 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (87.18%) - LeetSync

--- 52-n-queens-ii/n-queens-ii.cpp
+++ 52-n-queens-ii/n-queens-ii.cpp
@@ -1,34 +1,26 @@
 class Solution {
 public:
-    int helper(int &count, int row, int n, vector<int>& upperCol,
-                vector<int>& lowerDiagonal, vector<int>& upperDiagonal) {
-        if (row == n) {
-            return 1;
+        int a = 0;
+    int b;
+
+    void dfs(int r, int c, int d1, int d2) {
+        if (r == b) {
+            a++;
+            return;
         }
-        for (int col = 0; col < n; col++) {
-            if (upperCol[col] == 0 && lowerDiagonal[col + row] == 0 &&
-                upperDiagonal[(n - 1) + col - row] == 0) {
-                upperCol[col] = 1;
-                lowerDiagonal[col + row] = 1;
-                upperDiagonal[(n - 1) + col - row] = 1;
-                count+= helper(count, row + 1, n, upperCol, lowerDiagonal, upperDiagonal);
-                upperCol[col] = 0;
-                lowerDiagonal[col + row] = 0;
-                upperDiagonal[(n - 1) + col - row] = 0;
-            }
+
+        int m = ((1 << b) - 1) & ~(c | d1 | d2);
+        while (m > 0) {
+            int p = m & -m;
+            m -= p;
+            dfs(r + 1, c | p, (d1 | p) << 1, (d2 | p) >> 1);
         }
-        return 0;
     }
 
     int totalNQueens(int n) {
-        if (n <= 1) {
-            return n;
-        }
-        vector<int> upperCol(n, 0);
-        vector<int> lowerDiagonal(2 * n - 1, 0);
-        vector<int> upperDiagonal(2 * n - 1, 0);
-        int count = 0;
-        helper(count, 0, n, upperCol, lowerDiagonal, upperDiagonal);
-        return count;
+        a = 0;
+        b = n;
+        dfs(0, 0, 0, 0);
+        return a;
     }
 };
