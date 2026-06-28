1d1640122ebc02ef9c91aec68a6fc9e0c96568b1
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-06-28 18:54:06 +0530

    Time: 0 ms (100.00%) | Memory: 9.2 MB (60.93%) - LeetSync

--- 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.cpp
@@ -1,32 +1,25 @@
 class Solution {
 public:
-    void helper(int ind, int n, string& temp, string& digits, vector<string>& keypad,
-                vector<string>& ans) {
-        if(ind == n){
-            ans.push_back(temp);
-            return;
-        }
-
-        int indxKeypad = digits[ind]-'0';
-        string s = keypad[indxKeypad];
-        for(int i=0; i<s.size(); i++) {
-            temp.push_back(s[i]);
-            helper(ind+1, n, temp, digits, keypad, ans);
-            temp.pop_back();
-        }
-    }
-
     vector<string> letterCombinations(string digits) {
-        vector<string> keypad = {
+        if (digits.empty()) return {};
+        
+        vector<string> m = {
             "", "", "abc", "def", "ghi", "jkl", "mno", "pqrs", "tuv", "wxyz"
         };
-        vector<string> ans;
-        string temp = "";
-        int n = digits.size();
-        if(n==0){
-            return ans;
+        
+        vector<string> r = {""};
+        
+        for (char d : digits) {
+            vector<string> t;
+            string s = m[d - '0'];
+            for (string x : r) {
+                for (char c : s) {
+                    t.push_back(x + c);
+                }
+            }
+            r = t;
         }
-        helper(0, n, temp, digits, keypad, ans);
-        return ans;
+        
+        return r;
     }
 };
