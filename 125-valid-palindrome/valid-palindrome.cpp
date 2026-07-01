b5a7ba21c304be0690d74e450fe38a6e0aa986c2
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-01 18:58:12 +0530

    Time: 0 ms (100.00%) | Memory: 9.9 MB (55.37%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,30 +1,20 @@
 class Solution {
 public:
-    bool helper(int ind, int n, string& s) {
-        if (ind >= n / 2) {
-            return true;
-        }
-
-        if (s[ind] != s[n - 1 - ind]) {
-            return false;
-        }
-
-        return helper(ind + 1, n, s);
-    }
     bool isPalindrome(string s) {
-        // if (s.size() <= 1) {
-        //     return true;
-        // }
-        string str = "";
-        for (auto it : s) {
-            if (it >= 'a' && it <= 'z' || it >= '0' && it <= '9')
-                str += it;
-            if (it >= 'A' && it <= 'Z')
-                str += (char)('a' + (it - 'A'));
+        int l = 0, r = s.length() - 1;
+        while (l < r) {
+            if (!isalnum(s[l])) {
+                l++;
+            } else if (!isalnum(s[r])) {
+                r--;
+            } else {
+                if (tolower(s[l]) != tolower(s[r])) {
+                    return false;
+                }
+                l++;
+                r--;
+            }
         }
-
-        int n = str.size();
-        cout << str << endl;
-        return helper(0, n, str);
+        return true;
     }
 };
