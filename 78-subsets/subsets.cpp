e62e3bca7104dbe1e9f1ad69349e381821a0b98e
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-08 13:18:32 +0530

    Time: 0 ms (100.00%) | Memory: 9.8 MB (82.23%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,25 +1,20 @@
 class Solution {
 public:
-    void helper(int ind, int n, vector<int> &nums, vector<int> &temp, vector<vector<int>> &ans) {
-        if(ind==n){
-            ans.push_back(temp);
-            return;
-        }
-        temp.push_back(nums[ind]);
-        helper(ind+1, n, nums, temp, ans);
-        temp.pop_back();
-
-        helper(ind+1, n, nums, temp, ans);
-    }
-    vector<vector<int>> subsets(vector<int>& nums) {
-        // unordered_set<vector<int>> st;
-        // int n=nums.size();
-        // helper(0,n,st,nums);
-        // for(auto it:)
+    vector<vector<int>> subsets(vector<int>& N) {
         vector<vector<int>> ans;
-        vector<int> temp;
-        int n = nums.size();
-        helper(0, n, nums, temp, ans);
+        int sz = N.size();
+        int total = 1 << sz;
+
+        for (int i = 0; i < total; i++) {
+            vector<int> cur;
+            for (int j = 0; j < sz; j++) {
+                if ((i >> j) & 1) {
+                    cur.push_back(N[j]);
+                }
+            }
+            ans.push_back(cur);
+        }
+
         return ans;
     }
 };
