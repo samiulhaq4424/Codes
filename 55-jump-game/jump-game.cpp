40603121b56161441105ba4615611d9f5e1ff23a
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-13 17:42:58 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (58.60%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -3,19 +3,13 @@ public:
     bool canJump(vector<int>& nums) {
         int maxReach = 0;
         int n = nums.size();
-        
+
         for (int i = 0; i < n; i++) {
-            if (i > maxReach) {
-                return false;
-            }
-            
+            if (i > maxReach) return false;
             maxReach = max(maxReach, i + nums[i]);
-            
-            if (maxReach >= n - 1) {
-                return true;
-            }
+            if (maxReach >= n - 1) return true;
         }
-        
+
         return true;
     }
 };
