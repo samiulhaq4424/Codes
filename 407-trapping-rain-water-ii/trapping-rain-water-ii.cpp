c733ec48d01dde62021d5b355d4ee82ec1ac80dd
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-13 23:51:59 +0530

    Time: 24 ms (78.63%) | Memory: 17.7 MB (81.40%) - LeetSync

--- 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
+++ 407-trapping-rain-water-ii/trapping-rain-water-ii.cpp
@@ -1,38 +1,54 @@
 class Solution {
 public:
-    int trapRainWater(vector<vector<int>>& grid) {
-        int rows = grid.size();
-        int cols = grid[0].size();
-        priority_queue<tuple<int, int, int>, vector<tuple<int, int, int>>, greater<>> heap;
-        vector<vector<bool>> visited(rows, vector<bool>(cols, false));
-        
-        for (int i = 0; i < rows; i++) {
-            for (int j = 0; j < cols; j++) {
-                if (i == 0 || j == 0 || i == rows - 1 || j == cols - 1) {
-                    heap.emplace(grid[i][j], i, j);
+    int trapRainWater(vector<vector<int>>& heightMap) {
+        if (heightMap.empty() || heightMap[0].empty()) return 0;
+
+        int m = heightMap.size();
+        int n = heightMap[0].size();
+
+        priority_queue<pair<int, pair<int, int>>, 
+        vector<pair<int, pair<int, int>>>, 
+        greater<pair<int, pair<int, int>>>> pq;
+
+        vector<vector<bool>> visited(m, vector<bool>(n, false));
+
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || i == m - 1 || j == 0 || j == n - 1) {
+                    pq.push({heightMap[i][j], {i, j}});
                     visited[i][j] = true;
                 }
             }
         }
-        
-        vector<pair<int, int>> directions = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-        int totalWater = 0;
-        
-        while (!heap.empty()) {
-            auto [height, row, col] = heap.top();
-            heap.pop();
-            
-            for (auto [dr, dc] : directions) {
-                int newRow = row + dr;
-                int newCol = col + dc;
-                if (newRow >= 0 && newCol >= 0 && newRow < rows && newCol < cols && !visited[newRow][newCol]) {
-                    visited[newRow][newCol] = true;
-                    totalWater += max(0, height - grid[newRow][newCol]);
-                    heap.emplace(max(height, grid[newRow][newCol]), newRow, newCol);
+
+        int water = 0;
+        int max_height = 0;
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+
+        while (!pq.empty()) {
+            auto curr = pq.top();
+            pq.pop();
+
+            int height = curr.first;
+            int r = curr.second.first;
+            int c = curr.second.second;
+
+            max_height = max(max_height, height);
+
+            for (int i = 0; i < 4; i++) {
+                int nr = r + dirs[i][0];
+                int nc = c + dirs[i][1];
+
+                if (nr >= 0 && nr < m && nc >= 0 && nc < n && !visited[nr][nc]) {
+                    visited[nr][nc] = true;
+                    if (heightMap[nr][nc] < max_height) {
+                        water += max_height - heightMap[nr][nc];
+                    }
+                    pq.push({heightMap[nr][nc], {nr, nc}});
                 }
             }
         }
-        
-        return totalWater;
+
+        return water;
     }
 };
