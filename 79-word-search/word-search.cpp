9ffaaefe6f359639c77ad978a961f4d0ed7c0e33
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-16 13:33:33 +0530

    Time: 187 ms (89.39%) | Memory: 10.5 MB (65.48%) - LeetSync

--- 79-word-search/word-search.cpp
+++ 79-word-search/word-search.cpp
@@ -1,51 +1,36 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
-    bool helper(int ind, int X, int Y, int wordSize, int n, int m, int dirX[],
-                int dirY[], vector<vector<bool>>& visited, string& word,
-                vector<vector<char>>& board) {
-        // current cell must match current character
-        if (board[X][Y] != word[ind]) return false;
+    bool dfs(vector<vector<char>>& grid, string& word, int idx, int r, int c, int m, int n) {
+        if (idx == word.length()) return true;
+        if (r < 0 || r >= m || c < 0 || c >= n || grid[r][c] != word[idx]) return false;
 
-        // base case: last character matched
-        if (ind == wordSize - 1) return true;
+        char temp = grid[r][c];
+        grid[r][c] = '#';
 
-        visited[X][Y] = true;
-
+        int dirs[4][2] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
         for (int i = 0; i < 4; i++) {
-            int nextX = X + dirX[i];
-            int nextY = Y + dirY[i];
-            if (nextX >= 0 && nextY >= 0 && nextX < n && nextY < m &&
-                !visited[nextX][nextY]) {
-                if (helper(ind + 1, nextX, nextY, wordSize, n, m, dirX, dirY,
-                           visited, word, board)) {
-                    return true;
-                }
+            if (dfs(grid, word, idx + 1, r + dirs[i][0], c + dirs[i][1], m, n)) {
+                return true;
             }
         }
 
-        visited[X][Y] = false; // backtrack
+        grid[r][c] = temp;
         return false;
     }
 
+    public:
     bool exist(vector<vector<char>>& board, string word) {
-        int dirX[] = {1, 0, 0, -1};
-        int dirY[] = {0, -1, 1, 0};
-        int n = board.size();
-        int m = board[0].size();
-        int wordSize = word.size();
-        vector<vector<bool>> visited(n, vector<bool>(m, false));
+        int m = board.size();
+        int n = board[0].size();
 
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (helper(0, i, j, wordSize, n, m, dirX, dirY, visited, word,
-                           board)) {
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (board[i][j] == word[0] && dfs(board, word, 0, i, j, m, n)) {
                     return true;
                 }
             }
         }
+
         return false;
     }
 };
