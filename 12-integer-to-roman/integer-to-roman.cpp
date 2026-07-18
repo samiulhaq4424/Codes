fafc4a0dcd62a8b51fb1b534d61a1cbc6c067c12
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-18 16:32:18 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (40.10%) - LeetSync

--- 12-integer-to-roman/integer-to-roman.cpp
+++ 12-integer-to-roman/integer-to-roman.cpp
@@ -1,26 +1,21 @@
-#include <bits/stdc++.h>
-using namespace std;
-
 class Solution {
 public:
     string intToRoman(int num) {
-        
-        vector<int> values = 
-        {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        
-        vector<string> symbols = 
-        {"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        vector<pair<int, string>> roman = {
+            {1000, "M"}, {900, "CM"}, {500, "D"}, {400, "CD"},
+            {100, "C"},  {90, "XC"},  {50, "L"},  {40, "XL"},
+            {10, "X"},   {9, "IX"},   {5, "V"},   {4, "IV"},
+            {1, "I"}
+        };
 
-        string result = "";
-
-        for (int i = 0; i < values.size(); i++) {
-
-            while (num >= values[i]) {
-                result += symbols[i];   
-                num -= values[i];      
+        string ans = "";
+        for (auto& p : roman) {
+            while (num >= p.first) {
+                ans += p.second;
+                num -= p.first;
             }
         }
 
-        return result;
+        return ans;
     }
 };
