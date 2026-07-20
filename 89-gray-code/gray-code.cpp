89d3035f691969ab5e2890bd9aec3984d2e60b6c
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-20 19:17:51 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (86.03%) - LeetSync

--- 89-gray-code/gray-code.cpp
+++ 89-gray-code/gray-code.cpp
@@ -1,16 +1,10 @@
 class Solution {
 public:
     vector<int> grayCode(int n) {
-        vector<int> result;
-        result.push_back(0);
-        
-        for (int i = 0; i < n; i++) {
-            int size = result.size();
-            for (int j = size - 1; j >= 0; j--) {
-                result.push_back(result[j] | (1 << i));
-            }
+        vector<int> ans(1 << n);
+        for (int i = 0; i < (1 << n); i++) {
+            ans[i] = i ^ (i >> 1);
         }
-        
-        return result;
+        return ans;
     }
 };
