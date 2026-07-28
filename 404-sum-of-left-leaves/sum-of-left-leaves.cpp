47a66c62b2a45160288670f02fd1685ab2423238
Samiul Haque <38177252+samiulhaq4424@users.noreply.github.com>
2026-07-28 20:24:32 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (24.44%) - LeetSync

--- 404-sum-of-left-leaves/sum-of-left-leaves.cpp
+++ 404-sum-of-left-leaves/sum-of-left-leaves.cpp
@@ -13,9 +13,16 @@ class Solution {
 public:
     int sumOfLeftLeaves(TreeNode* root) {
         if (!root) return 0;
+
         int sum = 0;
-        if (root->left && !root->left->left && !root->left->right)
+
+        if (root->left && !root->left->left && !root->left->right) {
             sum += root->left->val;
-        return sum + sumOfLeftLeaves(root->left) + sumOfLeftLeaves(root->right);
+        }
+
+        sum += sumOfLeftLeaves(root->left);
+        sum += sumOfLeftLeaves(root->right);
+
+        return sum;
     }
 };
